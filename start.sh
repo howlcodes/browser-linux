6b34d45003a06369c8894e3d3ab4acc0dc00c977
howlcodes <howlmakesedits@gmail.com>
2025-12-05 09:16:10 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# Start VNC server on display :1
+vncserver :1 -geometry 1280x720 -depth 24
+
+# Launch noVNC server with websockify
+cd ~/noVNC
+./utils/launch.sh --vnc localhost:5901 --listen 6080
